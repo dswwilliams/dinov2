0fd16aaf50ebff0368a90a99340b74069a6e89e8
David Williams <dw@robots.ox.ac.uk>
2024-02-02 21:01:29 +0000

    adding lora rank as variable

--- dinov2/models/vision_transformer.py
+++ dinov2/models/vision_transformer.py
@@ -103,6 +103,7 @@ class DinoVisionTransformer(nn.Module):
         self.num_register_tokens = num_register_tokens
         self.interpolate_antialias = interpolate_antialias
         self.interpolate_offset = interpolate_offset
+        self.lora_rank = lora_rank
 
         self.patch_embed = embed_layer(img_size=img_size, patch_size=patch_size, in_chans=in_chans, embed_dim=embed_dim)
         num_patches = self.patch_embed.num_patches
